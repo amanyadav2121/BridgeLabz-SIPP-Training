cd4b78d59f48a8ec18ac6bfe0e4068230baa6821
AMAN YADAV <156089872+amanyadav2121@users.noreply.github.com>
2025-06-11 09:57:02 +0530

    Add files via upload

---
+++ Assignment 1/Area_of_a_Circle.java
@@ -0,0 +1,12 @@
+//Area of a Circle
+
+import java.util.Scanner;
+
+public class Area_of_a_Circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int radius= sc.nextInt();
+        double Area = 3.147 * (radius*radius);
+        System.out.println("area is "+Area);
+    }
+}
